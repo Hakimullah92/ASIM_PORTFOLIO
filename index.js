259c6cca6eb9edc97a0f0f0a9e3884f1cb33ad54
Hakim Ullah <106232159+Hakimullah92@users.noreply.github.com>
2023-04-05 16:33:39 +0500

    Adding logo ,whatsapp message functionality and customize the navbar

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // Navlink active Functionality Start
 
-let navlinks=document.querySelectorAll(".nav-link");
+let navlinks=document.querySelectorAll(".nav-link"),
+     navbarlist=document.querySelector("#navbarNav"),
+     menuicon=document.querySelector("#menu"),
+     closeicon=document.querySelector("#close"),
+     main=document.getElementById("main");
 console.log(navlinks);
 
 navlinks.forEach((item)=>{
@@ -10,5 +14,14 @@ navlinks.forEach((item)=>{
      });
      item.classList.add('active');
   });
+});
+
+menuicon.addEventListener("click",()=>{
+  navbarlist.classList.add("active");
+  main.classList.add("active");
+});
+closeicon.addEventListener("click",()=>{
+ navbarlist.classList.remove("active");
+ main.classList.remove("active");
 });
 // Navlink active Functionality End
